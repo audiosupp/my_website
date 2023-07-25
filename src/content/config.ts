35de0c25f177e7bdcca23d5c27caddcc1d14ff61
audiosupp <ivangerasimyukmail@gmail.com>
2023-07-25 22:40:49 +0300

    c6

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,14 +8,24 @@ const postsCollection = defineCollection({
       pubDate: z.date(),
       description: z.string(),
       author: z.string(),
-      image: z.object({
-        url: z.string(),
-        alt: z.string()
-      }),
       tags: z.array(z.string())
     })
 });
+
+const blogCollection = defineCollection({
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    cover: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    coverAlt: z.string(),
+  }),
+});
+
+
 // Export a single `collections` object to register your collection(s)
 export const collections = {
   posts: postsCollection,
+  blog: blogCollection
 };
+
